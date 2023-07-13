9eca9e43be633eaf30497c7b6d53fb166bb9b5db
tejubn <mail2tejubn06@gmail.com>
2023-07-13 14:05:42 +0530

    Day-3

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
-const heading = React.createElement("h1", { id: "heading" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "grandchild1 "),
-    React.createElement("h1", {}, "grandchild2"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "grandchild1"),
-    React.createElement("h1", {}, "grandchild2"),
-  ]),
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+
+const Title=()=>(
+    <div>hello</div>
+);
+const HeadingComponent=function (){
+    return (
+    <h1>hai hello
+    <Title/>
+</h1>)};
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>)
+
+
+// const heading=React.createElement("h1",{id:"heading"},"hi teju");
+// // when its rendered to DOM then it becomes HTML element
+// const jsxHeading=<h1 id="heading">JSX heading</h1>
+// //JSX syntax
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading)
+// what is JSX ? it is a JS syntax which helps to easily create react element
